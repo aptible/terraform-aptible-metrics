a8a0b71f31fc6fa53b24896dd929ca0ca50bf25e
Josh Raker <josh.raker@aptible.com>
2022-10-11 06:52:13 -0400

    WIP: Use null variable defaults to have the provider use its default values when none is provided

--- modules/aptible/shared_variables.tf
+++ modules/aptible/shared_variables.tf
@@ -2,6 +2,7 @@
 variable "metrics_environment" {
   description = "The handle of the environment to send metrics to. This can be one of the drain_environments."
   type        = string
+  nullable    = false
 }
 
 variable "drain_environments" {
@@ -14,6 +15,7 @@ variable "grafana_handle" {
   description = "The handle to use for the Grafana App."
   type        = string
   default     = "grafana"
+  nullable    = false
 }
 
 variable "grafana_endpoint_domain" {
@@ -26,64 +28,68 @@ variable "grafana_db_user" {
   description = "The user that Grafana will use to access the PostgreSQL Database. This user is created and granted the necessary permissions on the Database."
   type        = string
   default     = "grafana"
+  nullable    = false
 }
 
 variable "grafana_image_tag" {
   description = "The grafana/grafana image tag (i.e. version) that the app will use."
   type        = string
   default     = "latest"
+  nullable    = false
 }
 
 variable "grafana_container_count" {
   description = "The number of Grafana App containers to deploy."
   type        = number
-  default     = 1
+  default     = null
 }
 
 variable "grafana_container_size" {
   description = "The size (in MB) of the Grafana App containers."
   type        = number
-  default     = 1024
+  default     = null
 }
 
 variable "grafana_container_profile" {
   description = "The instance profile of the Grafana App containers."
   type        = string
-  default     = "m4"
+  default     = null
 }
 
 variable "influx_handle" {
   description = "The handle to use for the InfluxDB Database used to store metrics."
   type        = string
   default     = "influx"
+  nullable    = false
 }
 
 variable "influx_container_size" {
   description = "The size (in MB) of the InfluxDB metrics Database container."
   type        = number
-  default     = 1024
+  default     = null
 }
 
 variable "influx_container_profile" {
   description = "The instance profile of the InfluxDB metrics Database container."
   type        = string
-  default     = "m4"
+  default     = null
 }
 
 variable "postgres_handle" {
   description = "The handle to use for the PostgreSQL Database used for storing Grafana data."
   type        = string
   default     = "pg-grafana"
+  nullable    = false
 }
 
 variable "postgres_container_size" {
   description = "The size (in MB) of the PostgreSQL Grafana Database container."
   type        = number
-  default     = 1024
+  default     = null
 }
 
 variable "postgres_container_profile" {
   description = "The instance profile of the PostgreSQL Grafana Database container."
   type        = string
-  default     = "m4"
+  default     = null
 }
--- modules/grafana/variables.tf
+++ modules/grafana/variables.tf
@@ -1,13 +1,17 @@
 variable "grafana_url" {
   description = "The base URL of the Grafana instance to configure."
   type        = string
+  nullable    = false
 }
 
 variable "grafana_auth" {
   description = "Credentials to access Grafana in the form of user:password or an API key."
   type        = string
+  nullable    = false
 }
 
 variable "influx_data_source_uid" {
   description = "The UID of the InfluxDB data source to use when creating Grafana resources."
+  type        = string
+  nullable    = false
 }
