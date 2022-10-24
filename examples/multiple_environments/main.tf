16a923d45a391182362127d533d98214d54bb919
Josh Raker <48493233+joshraker@users.noreply.github.com>
2022-10-24 19:09:32 -0400

    Make the repo more terraform registry friendly (#2)

--- examples/multiple_environments/main.tf
+++ examples/multiple_environments/main.tf
@@ -1,10 +1,5 @@
-# ------------------------------------------------------------------------------
-# Collecting metrics for multiple Environments is also a simple task. This
-# configuration will host the metrics in the my-metrics Environment and will
-# collect metrics from my-metrics, my-env, and my-other-env Environments.
-# ------------------------------------------------------------------------------
 module "aptible_metrics" {
-  # Use the git source in your own templates
+  # Use the git or terraform registry source in your own templates
   # source = "git::git@github.com:aptible/terraform-aptible-metrics.git?ref=v1.0.0"
   source              = "../../"
   metrics_environment = "my-metrics"
---
+++ modules/aptible/README.md
@@ -0,0 +1,4 @@
+# Aptible Metrics Resources
+
+This module is responsible for managing the Aptible resources (and the Grafana
+data source) necessary to collect and store container metrics.
