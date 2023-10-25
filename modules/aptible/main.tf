6f167693c61c487ebfb7940bf75c8c0910637099
John Turner <john.turner@aptible.com>
2023-10-25 13:24:04 -0400

    Updating to use milli_cpu_limit

--- modules/aptible/main.tf
+++ modules/aptible/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aptible = {
       source  = "aptible/aptible"
-      version = "~> 0.5.1"
+      version = "~> 0.8.1"
     }
     grafana = {
       source  = "grafana/grafana"
